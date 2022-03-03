644ee629f80862ed72afd0a411b13b9e23445ce0
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-03 22:25:08 +0300

    ft: add bg image

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var hambuger = document.querySelector("#menubar");
 var menubar = document.querySelector(".menu-bar");
 var lowerNav = document.querySelector("#navUl");
 var nav = document.querySelector(".nav");
+var overlay = document.querySelector(".bkg-overlay");
 
 hambuger.addEventListener("click", function () {
   hambuger.classList.toggle("active");
   lowerNav.classList.toggle("is-active");
   nav.classList.toggle("changebg");
+  overlay.classList.toggle("mvDown");
 });
