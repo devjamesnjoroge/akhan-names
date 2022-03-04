293e42444c76d0b8d1f917fcb08a5c67c871a422
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-04 13:01:46 +0300

    ft: alert user akan name logical operators

--- app.js
+++ app.js
@@ -23,16 +23,19 @@ form.addEventListener("submit", function (submit) {
 
   if ("gender" === gender) {
     alert("Please select a gender");
+    event.preventDefault();
   } else if (date <= 0 || date > 31) {
     alert("invalid date");
+    event.preventDefault();
   } else if ("month" === month) {
     alert("Please select a month");
+    event.preventDefault();
   } else if (year < 1900 || year > 2022) {
     alert("invalid year");
+    event.preventDefault();
   } else {
     calculator(gender, month, date, year);
   }
-  event.preventDefault();
 });
 
 // Business Logic
@@ -45,5 +48,62 @@ function calculator(gender, month, date, year) {
   let answer =
     (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7;
   let ansRnd = Math.round(answer);
-  alert(ansRnd);
+  let akanMale = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yow",
+    "Kofi",
+    "Kwame",
+  ];
+  let akanFmale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  let switchAns = ansRnd.toString() + gender;
+
+  switch (switchAns) {
+    case "1male":
+      alert("Your Akan name is " + akanMale[0]);
+      break;
+    case "1female":
+      alert("Your Akan name is " + akanFmale[0]);
+      break;
+    case "2male":
+      alert("Your Akan name is " + akanMale[1]);
+      break;
+    case "2female":
+      alert("Your Akan name is " + akanFmale[1]);
+      break;
+    case "3male":
+      alert("Your Akan name is " + akanMale[2]);
+      break;
+    case "3female":
+      alert("Your Akan name is " + akanFmale[2]);
+      break;
+    case "4male":
+      alert("Your Akan name is " + akanMale[3]);
+      break;
+    case "4female":
+      alert("Your Akan name is " + akanFmale[3]);
+      break;
+    case "5male":
+      alert("Your Akan name is " + akanMale[4]);
+      break;
+    case "5female":
+      alert("Your Akan name is " + akanFmale[4]);
+      break;
+    case "6male":
+      alert("Your Akan name is " + akanMale[5]);
+      break;
+    case "6female":
+      alert("Your Akan name is " + akanFmale[5]);
+      break;
+    case "7male":
+      alert("Your Akan name is " + akanMale[7]);
+      break;
+    case "7female":
+      alert("Your Akan name is " + akanFmale[7]);
+      break;
+    default:
+      alert("An error occurred, details sent to server");
+  }
 }
