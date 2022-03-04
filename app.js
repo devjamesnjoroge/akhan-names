eaf6c54e5b7ea02509a35ae96ddffec1ef634107
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-04 12:28:40 +0300

    ft: add akan math function

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ form.addEventListener("submit", function (submit) {
   var month = document.querySelector("select#month").value;
   var date = parseInt(document.querySelector("input#date").value);
   var year = parseInt(document.querySelector("input#year").value);
+
   if ("gender" === gender) {
     alert("Please select a gender");
   } else if (date <= 0 || date > 31) {
@@ -29,11 +30,20 @@ form.addEventListener("submit", function (submit) {
   } else if (year < 1900 || year > 2022) {
     alert("invalid year");
   } else {
-    calculator();
+    calculator(gender, month, date, year);
   }
   event.preventDefault();
 });
 
-
 // Business Logic
-
+function calculator(gender, month, date, year) {
+  var yearArray = year.toString().split("");
+  var cc = parseInt([yearArray[0], yearArray[1]].join(""));
+  var yy = parseInt([yearArray[2], yearArray[3]].join(""));
+  var mm = parseInt(month);
+  var dd = date;
+  let answer =
+    (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7;
+  let ansRnd = Math.round(answer);
+  alert(ansRnd);
+}
